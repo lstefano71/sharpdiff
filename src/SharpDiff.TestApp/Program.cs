8e47785c1b3522fcf32b25eaaec5e671c5dd4a6a
Lysann Kessler <lysann.kessler@student.hpi.uni-potsdam.de>
2012-02-04 16:42:53 +0100

    added simplisitc test programm that parses a diff file and prints some high level information about it

---
+++ src/SharpDiff.TestApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using SharpDiff;
+using SharpDiff.FileStructure;
+
+namespace SharpDiff.TestApp {
+    class Program {
+        static void Main(string[] args) {
+            if(args.Length < 1) {
+                Console.WriteLine("Usage: SharpDiff.TestApp.exe file.diff");
+                return;
+            }
+
+            string fileName = args[0];
+            string content = System.IO.File.ReadAllText(fileName);
+            IEnumerable<Diff> diffs = Differ.Load(content);
+            Console.WriteLine("{0} diffs", diffs.Count());
+
+            foreach(var diff in diffs) {
+                Console.WriteLine("{0} chunks", diff.Chunks.Count());
+                foreach(var chunk in diff.Chunks) {
+                    Console.WriteLine("  {0} snippets", chunk.Snippets.Count());
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
